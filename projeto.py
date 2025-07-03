e527509a152f36ab71279744434f8bcc0a2c7fe5
Datto6 <61701645+Datto6@users.noreply.github.com>
2025-07-03 15:09:38 -0300

    versao final final agora

--- projeto.py
+++ projeto.py
@@ -1,6 +1,6 @@
 import random as r
 
-#funcao para printar matriz bonita
+#função para printar matriz bonita
 def mostramatriz(entrada):
     for i in range(len(entrada)):
         linha=""
@@ -8,7 +8,7 @@ def mostramatriz(entrada):
             linha+=" "+str(entrada[i][j])  #usa str para concatenar a matriz que era int antes
         print("[",linha, "]")
 
-#inicializar matriz real e a que o usuario ve
+#inicializar matriz real e a que o usuario vê, ambas vazias
 matriz=[]
 amostradinha=[]
 for i in range(6):
@@ -16,30 +16,40 @@ for i in range(6):
     amostradinha.append(['?']*6)
 
 
-# colocar 0s e 1s em lugares aleatorios(1 e o satelite)
+# colocar 0s e 1s em lugares aleatorios(1 é o satelite)
 satelites=0 #Controla qntd de satelites
 while satelites<4:
     i=r.randint(0,5)
     j=r.randint(0,5)
-    #Condicao impede redundancia(satelites em lugares distintos)
+    #Condição impede redundância(satelites em lugares distintos)
     if matriz[i][j]!=1:
         matriz[i][j]=1
         satelites+=1
 
-# loop de acoes do jogo de fato
+# loop de ações do jogo de fato
 encontrados=0
 tentativas=0 #conta o numero de tentativas
-pontosfeitos=[] #guarda par coordenada em tupla p o usuario n entrar um ponto duas vezes e mudar de X--> . 
+pontosfeitos=[] #guarda par coordenada em tupla p o usuário n entrar um ponto duas vezes e mudar ?-->X--> . 
+
 while encontrados<4:
     print("Aqui o tabuleiro:")
     mostramatriz(amostradinha) #printa resultado de rodada anterior ou rodada 0 na 1 vez
-    linha=int(input("Fala qual linha voce acha que o satelite ta (numero de 0 a 5)"))
-    coluna=int(input("Agora coluna"))
+
+    try: #so no caso do user não botar numero, ou dar numero fora do intervalo entre 0 e 5 inclusive
+        linha=int(input("Fala qual linha voce acha que o satelite ta (numero de 0 a 5)"))
+        coluna=int(input("Agora coluna"))
+    except:
+        print("Voce provavelmente escreveu uma letra em vez de numero. Tenta de novo")
+        linha=int(input("Fala qual linha voce acha que o satelite ta (numero de 0 a 5)"))
+        coluna=int(input("Agora coluna"))
+
     while linha>5 or coluna>5 or linha<0 or coluna<0:
-        print("Input errado, entre 0 e 5.")
+        print("Input errado, numero entre 0 e 5.")
         linha=int(input("Linha."))
         coluna=int(input("Coluna"))
+
     #Avaliar se o usuário acertou ou errou
+
     if matriz[linha][coluna]==1 and (linha,coluna) not in pontosfeitos:
         amostradinha[linha][coluna]="X"
         matriz[linha][coluna]=-1
@@ -48,7 +58,7 @@ while encontrados<4:
         print("Voce ja deu essa coordenada, + uma tentativa pelo amor de deus ")
     else: #errou
         amostradinha[linha][coluna]="."
-    pontosfeitos.append((linha,coluna))
+    pontosfeitos.append((linha,coluna)) #usa tupla porque nao vai precisar mudar, e economiza memoria
     tentativas+=1
 
 if encontrados==4: #printar numero de tentativas e aproveitamento porque sim
